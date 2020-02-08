d18198aa4de663281498e98d5064bfccb1acb079
znixonz <znixonz@yahoo.com>
2020-02-08 22:11:04 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/male.png'),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
