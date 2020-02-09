2e0653e1a330857c994b17776a6f1ae74c9409fa
znixonz <znixonz@yahoo.com>
2020-02-09 18:36:49 +0800

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,12 +16,69 @@ class MyApp extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/male.png'),
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/male.png'),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
               ),
-            )
+            ),
+            Text(
+              "Nixon",
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+60 123 456 789",
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                )),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "example@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ))
           ],
         )),
       ),
